301b3a12f1aeaa89af7675af7b9ea52f6152ce00
Badgie <jonaskhansen@gmail.com>
2021-11-09 11:51:59 +0100

    gather data for each dir to save mem

--- analyzer/full.py
+++ analyzer/full.py
@@ -9,34 +9,38 @@ import numpy as np
 
 
 def full_run(_dir: str):
-    print('Parsing input dir')
-    data = _parse_input(_dir)
-    print('Parsing data')
-    res = [AnalyzedData(bench_name, bench_data) for bench_name, bench_data in data.items()]
     graph = GraphGenerator()
 
-    for r in res:
-        path = f'{_dir}/{r.name}/processed'
+    for _d in os.listdir(_dir):
+        d = f'{_dir}/{_d}'
+        print(f'Parsing dir {d}')
+        data = _parse_input(d)
+        print('Parsing data')
+        res = [AnalyzedData(bench_name, bench_data) for bench_name, bench_data in data.items()]
+        del data
 
-        if not os.path.isdir(path):
-            os.makedirs(path)
+        for r in res:
+            path = f'{d}/processed'
 
-        print(f'Writing {r.name} JSON')
-        with open(f'{path}/{r.name}.json', 'w') as f:
-            f.write(json.dumps(r.to_json(), indent=4))
+            if not os.path.isdir(path):
+                os.makedirs(path)
 
-        print(f'Generating {r.name} graphs')
-        power_j_total = r.collect_metrics_by_zone('power_j', 'total')
+            print(f'Writing {r.name} JSON')
+            with open(f'{path}/{r.name.split("/")[-1]}.json', 'w') as f:
+                f.write(json.dumps(r.to_json(), indent=4))
 
-        x = list(range(1, len(r.run_metrics) + 1))
-        graph.plot(power_j_total, x, f'{path}/power_j_total.png', 'Power (Joules)', r.name)
+            print(f'Generating {r.name} graphs')
+            power_j_total = r.collect_metrics_by_zone('power_j', 'total')
 
-        graph.bar({zone: np.average(value) for zone, value in power_j_total.items()},
-                  f'{path}/power_j_overall_avg', f'Power (Joules, avg - {len(r.run_metrics)} runs)', 'RAPL Zone', r.name)
+            x = list(range(1, len(r.run_metrics) + 1))
+            graph.plot(power_j_total, x, f'{path}/power_j_total.png', 'Power (Joules)', r.name)
+
+            graph.bar({zone: np.average(value) for zone, value in power_j_total.items()},
+                      f'{path}/power_j_overall_avg', f'Power (Joules, avg - {len(r.run_metrics)} runs)', 'RAPL Zone', r.name)
 
     print('Done')
 
 
 def _parse_input(_dir: str) -> dict:
-    return {d.split('/')[-1]: util.parse_input_paths([f'{_dir}/{d}']) for d in os.listdir(_dir)}
+    return {_dir: util.parse_input_paths([_dir])}
 
